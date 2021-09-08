e7178550946ffdc911c30e7f6c22f80d8a892bee
Ali Rahhal <arahal81@gmail.com>
2021-09-09 02:48:22 +0300

    api tester

--- api_tester.py
+++ api_tester.py
@@ -1,10 +1,10 @@
 import fire
 import requests
 
-API_HOST = "http://localhost:8000"
-RESOURCE_URI = "things"
-USERNAME = "admin"
-PASSWORD = "admin"
+API_HOST = "https://cookie-stand-api-lab34.herokuapp.com/"
+RESOURCE_URI = "cookie_stands"
+USERNAME = "ali"
+PASSWORD = "123456"
 
 
 class ApiTester:
